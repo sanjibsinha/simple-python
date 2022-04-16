1853a2bcfee7db2a46c61314249755a935865c42
sanjibsinha <sanjib12sinha@gmail.com>
2022-04-16 20:07:41 +0530

    No rain?

--- tuple-example.py
+++ tuple-example.py
@@ -6,9 +6,20 @@ secondTuple = (1, 2, 3)
 print(id(secondTuple))
 print(type(secondTuple))
 
+mixedTuple = firstTuple + secondTuple
+
+print(mixedTuple)
+print(id(mixedTuple))
+print(type(mixedTuple))
+
+
+
 '''
 139762668549608
 <class 'tuple'>
 139762668550040
+<class 'tuple'>
+(1, 2, 3, 1, 2, 3)
+140682466415912
 <class 'tuple'>
 '''
