0cad52c133f56b4cb8fcbdf262a4e2f1446da2e5
sanjibsinha <sanjib12sinha@gmail.com>
2022-04-19 12:11:46 +0530

    why variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+type_your_name = input("What is your name?\n")
+
+print(type_your_name)
+
+length_of_a_name = len(type_your_name)
+
+print("Your name is " + str(length_of_a_name) + " characters.")
+
