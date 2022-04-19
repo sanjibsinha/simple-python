6730c0d2a56923a4739d3a005f7c67e92493e639
sanjibsinha <sanjib12sinha@gmail.com>
2022-04-19 12:30:30 +0530

    variables output

--- variables.py
+++ variables.py
@@ -8,4 +8,18 @@ length_of_a_name_in_a_string = str(length_of_a_name)
 
 print("Your name is " + length_of_a_name_in_a_string + " characters.")
 
+'''
+sanjib
+Your name is 6 characters.
+'''
 
+'''
+TypeError                                 
+Traceback (most recent call last)
+~/Documents/development/simple-python/variables.py in <module>
+      5 length_of_a_name = len(type_your_name)
+      6 
+----> 7 print("Your name is " + length_of_a_name + " characters.")
+
+TypeError: must be str, not int
+'''
