32b9c570a924a35e923dfa09a1be947e75661803
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-22 12:31:10 +0530

    mathematical operations

---
+++ mathematical-operation.py
@@ -0,0 +1,80 @@
+x = 3 /2
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+1.5
+140597976235696
+<class 'float'>
+*********
+'''
+x = round(42 / 9)
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+5
+140597976233680
+<class 'int'>
+*********
+'''
+# we want to round it up
+x = 42 // 9
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+4
+9789056
+<class 'int'>
+*********
+'''
+# how many digits we want to round to
+x = round(42 / 9, 3)
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+4.667
+140597976235696
+<class 'float'>
+*********
+'''
+x = 43 % 7
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+1
+9788960
+<class 'int'>
+*********
+'''
+x = int(34.78)
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+'''
+34
+9790016
+<class 'int'>
+*********
+'''
+x = float(23)
+print(x)
+print(id(x))
+print(type(x))
+print("*********")
+
+'''
+23.0
+140597976235696
+<class 'float'>
+*********
+'''
