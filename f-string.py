caaf749f39dd82fb6051caa516bdb15619a24439
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-22 15:57:25 +0530

    f string

---
+++ f-string.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+
+score = 55
+weight = 65.45
+is_good = True
+
+print(f"Your score is {score}. Your weight is {weight} and you're {is_good}ly good.")
+
+'''
+Your score is 55. Your weight is 65.45 and you're Truely good.
+'''
