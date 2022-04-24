55229a025ee8569d5a2f8393ca5796e16d8ac5f6
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-24 07:31:42 +0530

    adding some tuple examples

--- tuple-example.py
+++ tuple-example.py
@@ -2,9 +2,19 @@ firstTuple = (1, 2, 3)
 print(id(firstTuple))
 print(type(firstTuple))
 
+'''
+139762668549608
+<class 'tuple'>
+'''
+
 secondTuple = (1, 2, 3)
 print(id(secondTuple))
 print(type(secondTuple))
+
+'''
+139762668550040
+<class 'tuple'>
+'''
 
 mixedTuple = firstTuple + secondTuple
 
@@ -12,14 +22,32 @@ print(mixedTuple)
 print(id(mixedTuple))
 print(type(mixedTuple))
 
-
-
 '''
-139762668549608
-<class 'tuple'>
-139762668550040
-<class 'tuple'>
 (1, 2, 3, 1, 2, 3)
 140682466415912
 <class 'tuple'>
+'''
+
+# we can nest tuple
+a_tuple = 1234, 5431, 'helicopter, you will fly!'
+another_tuple = a_tuple, 145, 'Dhoko', 569
+print(another_tuple)
+
+'''
+((1234, 5431, 'helicopter, you will fly!'), 145, 'Dhoko', 569)
+'''
+
+tuple_one = 1234, 5431, 'helicopter, you will fly!'
+print(tuple_one)
+
+'''
+(1234, 5431, 'helicopter, you will fly!')
+'''
+
+# sequence unpacking
+x, y, z = tuple_one
+print(x)
+
+'''
+1234
 '''
