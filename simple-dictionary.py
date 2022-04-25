f3229d280c23b2d5b37b7b30a18a98407241b3fd
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-25 11:56:56 +0530

    why integer is mutable

--- simple-dictionary.py
+++ simple-dictionary.py
@@ -1,7 +1,16 @@
 a = 1
+b = a + 2
 a_dict = {a : 'One'}
 print(a_dict)
 
+if a == 3:
+    a_dict[a] = a_dict.pop(a)
+    print(a_dict)
+else :
+    b = 3
+    a_dict[b] = a_dict.pop(a)
+    print(a_dict)
+    
 a = (2, 1)
 a_dict = {a : 'One'}
 print(a_dict)
@@ -9,6 +18,31 @@ print(a_dict)
 b = 'Two'
 a_dict[b] = a_dict.pop(a)
 print(a_dict)
+
+'''
+{1: 'One'}
+{3: 'One'}
+{(2, 1): 'One'}
+{'Two': 'One'}
+'''
+
+a_dict[b] = a_dict.pop(a[0])
+print(a_dict)
+
+
+'''
+KeyError                                  
+Traceback (most recent call last)
+~/Documents/development/simple-python/simple-dictionary.py in <module>
+      9 b = 'Two'
+     10 
+---> 11 a_dict[b] = a_dict.pop(a[0])
+     12 print(a_dict)
+     13 
+
+KeyError: 2
+'''
+
 
 
 
