3b397ee689581f92ecb72b65e0e75162f257337f
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-25 08:52:23 +0530

    adding some dict example

---
+++ simple-dictionary.py
@@ -0,0 +1,33 @@
+a = 1
+a_dict = {a : 'One'}
+print(a_dict)
+
+a = (2, 1)
+a_dict = {a : 'One'}
+print(a_dict)
+
+b = 'Two'
+a_dict[b] = a_dict.pop(a)
+print(a_dict)
+
+'''
+{1: 'One'}
+{(2, 1): 'One'}
+{'Two': 'One'}
+'''
+
+a_dict[b] = a_dict.pop(a[0])
+print(a_dict)
+
+'''
+KeyError                                  
+Traceback (most recent call last)
+~/Documents/development/simple-python/simple-dictionary.py in <module>
+      9 b = 'Two'
+     10 
+---> 11 a_dict[b] = a_dict.pop(a[0])
+     12 print(a_dict)
+     13 
+
+KeyError: 2
+'''
