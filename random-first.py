247283aa46f1a8a579225b9ce106f7c88314cc48
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-29 07:03:59 +0530

    Added random quote

--- random-first.py
+++ random-first.py
@@ -1,4 +1,5 @@
 import random
+from quote import quote
 
 random_float = random.random()
 print(random_float)
@@ -8,3 +9,26 @@ random_number = random.randint(1, 10)
 print(random_number)
 # 8
 
+random_quote = quote('friend',limit = 1)
+print(random_quote)
+
+'''
+[{'author': 'Albert Camus', 'book': '', 'quote': 'Don’t walk in front of me… 
+I may not followDon’t 
+walk behind me… I may not leadWalk beside me… just be my friend'}]
+'''
+
+for i in range(len(random_quote)):
+    print(random_quote[i]['author'])
+    
+# Albert Camus
+
+for i in range(len(random_quote)):
+    print(random_quote[i]['quote'])
+    
+'''
+Don’t walk in front of me… I may not follow
+Don’t walk behind me… I may not lead
+Walk beside me… just be my friend
+'''
+    
