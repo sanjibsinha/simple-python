f9188356fd3b724392528ed46982928e159a168f
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-02 12:52:36 +0530

    class an object second

---
+++ object-class-second-part/bear-object.py
@@ -0,0 +1,18 @@
+class Bear:
+    age = 10
+    
+    def introduce(self):
+        print("I am Bear.")
+    
+    def eat(self):
+        print("I am eating.")
+        
+child_bear = Bear()
+
+child_bear.introduce()
+child_bear.eat()
+
+'''
+I am Bear.
+I am eating.
+'''
