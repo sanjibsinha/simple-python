e15e9724cda7e15d605a219b88c3fbe88185643e
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-02 17:08:46 +0530

    class an object second modifying again

---
+++ object-class-second-part/desktop-lamp.py
@@ -0,0 +1,15 @@
+class DesktopLamp:
+    is_off = "off"
+    is_on = "on"
+    
+    def switch_on(self, is_on):
+        print(f"The lamp is {is_on}")
+        
+    def switch_off(self, is_off):
+        print(f"The lamp is {is_off}")
+        
+lamp = DesktopLamp()
+lamp.switch_on("switched on")
+# The lamp is switched on
+lamp.switch_off("switched off")
+# The lamp is switched off
