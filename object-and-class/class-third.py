1e52d3d23085a16d39080ecb964582a5a3a36ffb
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-06 16:16:34 +0530

    Third class again

--- object-and-class/class-third.py
+++ object-and-class/class-third.py
@@ -1,17 +1,20 @@
 class Bear:
-    age = 0
+    age = 10
+    time = 0
     
-    def get_the_present_age(self, age):
-        print(f"My age is {age}")
+    def get_the_present_age(self):
+        print(f"My age is {self.age}")
     
-    def grow_with_time(self, age):
-        print(f"I'm growing up. My age is {age}")
+    def grow_with_time(self, time):
+        self.age = self.age + time
+        print(f"I'm growing up. After {time} years, my age is {self.age}")
         
 bear = Bear()
-bear.get_the_present_age(10)
-bear.grow_with_time(11)
+bear.get_the_present_age()
+bear.grow_with_time(2)
 
 '''
 My age is 10
-I'm growing up. My age is 11
+I'm growing up. After 2 years, my age is 12
 '''
+
