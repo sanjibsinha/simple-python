1dddb33c1aa7295758a0e617b0bc98657ac780a3
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-07 07:14:04 +0530

    simple constructor

---
+++ object-and-class/class-constructor.py
@@ -0,0 +1,19 @@
+class Bear:
+    def __init__(self) -> None:
+        pass
+    
+
+class DeesktpLamp:
+    def __init__(self):
+        print("Nothing.")
+
+bear = Bear()
+print(bear)
+lamp = DeesktpLamp()
+print(lamp)
+
+'''
+<__main__.Bear object at 0x7f46a744b040>
+Nothing.
+<__main__.DeesktpLamp object at 0x7f46a73e67c0>
+'''
