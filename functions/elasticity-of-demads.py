67c701768ea030e436a21f6a241824b44308a280
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-12 08:19:37 +0530

    Solved

--- functions/elasticity-of-demads.py
+++ functions/elasticity-of-demads.py
@@ -1,6 +1,6 @@
 # elastic
 
-def find_increase_in_percentage_change_in_price(initial_price, percentage):
+""" def find_increase_in_percentage_change_in_price(initial_price, percentage):
     increase_in_percentage_change = initial_price * (1 + percentage/100)
     return int(increase_in_percentage_change)
 
@@ -13,36 +13,40 @@ def find_increase_in_percentage_change_in_quantity(initial_quantity, percentage)
 
 def find_decrease_in_percentage_change_in_quantity(initial_quantity, percentage):
     decrease_in_percentage_change = initial_quantity * (1 - percentage/100)
-    return int(decrease_in_percentage_change)
+    return int(decrease_in_percentage_change) """
 
 
 def find_elasticity_of_demand(initial_price, percentage_change_in_price, 
                     initial_quantity, percentage_change_in_quantity):
     elasticity_of_demand = (initial_quantity - percentage_change_in_quantity / 
-                            initial_quantity + percentage_change_in_quantity) / (
-                                initial_price - percentage_change_in_price / 
-                                initial_price + percentage_change_in_price
+                            initial_quantity + percentage_change_in_quantity) * (
+                                initial_price + percentage_change_in_price / 
+                                initial_price - percentage_change_in_price
                             )
     
     return elasticity_of_demand
 
-price_increase_percentage = find_increase_in_percentage_change_in_price(500, 10)
+""" price_increase_percentage = find_increase_in_percentage_change_in_price(500, 10)
 print(price_increase_percentage)
-price_decrease_percentage = find_decrease_in_percentage_change_in_price(500, 50)
+price_decrease_percentage = find_decrease_in_percentage_change_in_price(500, 10)
 print(price_decrease_percentage)
 
-quantity_increase_percentage = find_increase_in_percentage_change_in_quantity(60, 2)
+quantity_increase_percentage = find_increase_in_percentage_change_in_quantity(60, 50)
 print(quantity_increase_percentage)
 quantity_decrease_percentage = find_decrease_in_percentage_change_in_quantity(60, 10)
-print(quantity_decrease_percentage)
+print(quantity_decrease_percentage) """
 
 
-elasticity_of_demand = find_elasticity_of_demand(500, price_decrease_percentage, 
-                                                 60, quantity_increase_percentage)
+elasticity_of_demand = find_elasticity_of_demand(500, 450, 
+                                                 60, 90)
 
-if elasticity_of_demand > 1:
-    print(f"Elastic {elasticity_of_demand}")
-elif elasticity_of_demand == 1:
-    print(f"Unitary Elastic {elasticity_of_demand}")
+elasticity_of_demand_converted_to_integer = int(elasticity_of_demand)
+
+if elasticity_of_demand_converted_to_integer > 1:
+    print(f"Demand is Elastic and the elasticity coefficient is: {elasticity_of_demand_converted_to_integer}")
+elif elasticity_of_demand_converted_to_integer == 1:
+    print(f"Demand is Unitary Elastic and the elasticity coefficient is: {elasticity_of_demand_converted_to_integer}")
+elif elasticity_of_demand_converted_to_integer < 1:
+    print(f"Demand is Not Elastic and the elasticity coefficient is: {elasticity_of_demand_converted_to_integer}")
 else:
-    print(f"Not Elastic {elasticity_of_demand}")
+    print(f"The value {elasticity_of_demand_converted_to_integer}")
