eaf5e4ce01b87ab29382f629982172fa111f52ef
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-23 08:22:08 +0530

    pandas 2

--- first.py
+++ first.py
@@ -1,12 +1,12 @@
 import pandas as panda
 
-data_frame_of_numbers = panda.DataFrame({'label': ['John', 'Emily', 'Json'], 
-                      'value': [56, 89, 65]})
+data_frame_of_numbers = panda.DataFrame({'name': ['John', 'Emily', 'Json'], 
+                      'mark': [56, 89, 65]})
 
 print(data_frame_of_numbers)
 
 '''
-   label  value
+   name  mark
 0   John     56
 1  Emily     89
 2   Json     65
--- second.py
+++ second.py
@@ -1,11 +1,20 @@
-import numpy as np
+import pandas as panda
 
-from_one_to_5 = np.arange(1, 10)
-multi_dimensional_array = from_one_to_5.reshape(3, 3)
-print(multi_dimensional_array)
+data_frame_of_numbers = panda.DataFrame({'name': ['John', 'Emily', 'Json'], 
+                      'mark': [56, 89, 65]})
 
-'''
-[[1 2 3]
- [4 5 6]
- [7 8 9]]
+names = data_frame_of_numbers['name']
+print(names)
+marks = data_frame_of_numbers['mark']
+print(marks)
+
+''''
+0     John
+1    Emily
+2     Json
+Name: name, dtype: object
+0    56
+1    89
+2    65
+Name: mark, dtype: int64
 '''
