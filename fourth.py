24eeb429e6d863ccdfeb60b0c0b0b7cb554e48ba
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-25 13:32:43 +0530

    first commit

--- fourth.py
+++ fourth.py
@@ -2,13 +2,13 @@ import pandas as panda
 import matplotlib.pyplot as plt
 
 data_frame_of_numbers = panda.DataFrame({'name': ['John', 'Emily', 'Json', 'John', 'Emily', 'Json',], 
-                      'mark': [56, 89, 65, 69, 97, 68]})
+                      'mark': [56, 89, 65, 69, 97, 68]},)
 
 names = data_frame_of_numbers['name']
 
 aggregate_marks = data_frame_of_numbers.groupby('name').sum()
 
-plt.plot(aggregate_marks)
+plt.plot(aggregate_marks, 'g--')
 plt.show()
 
 
