02bd1d3070f492b9b842de6344bd2d5752d74a3b
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-27 09:54:11 +0530

    commit to maain

--- sixth.py
+++ sixth.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
 
-plt.plot((5, 10), (10, 35))
+def find_line_of_slope(x1, y1, x2, y2):
+    line_of_slope = (y2 - y1) /(x2 - x1)
+    print(f'Line of slope is: {line_of_slope}')
+    
+find_line_of_slope(0, 3, 4, 23)
+
+# Line of slope is: 5.0
+
+plt.plot((0, 3), (4, 23))
+
 plt.show()
