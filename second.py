2b36b2f34dba1a47a996fada5154ac69b453550b
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-30 08:32:32 +0530

    Beginners-one

--- second.py
+++ second.py
@@ -1,20 +1,18 @@
-input_one = input('Enter the value of the line-slope in positive integer: ')
-line_slope = int(input_one)
+class DesktopLamp:
+    is_off = "off"
+    is_on = "on"
+    
+    def switch_on(self, is_on):
+        print(f"The lamp is {is_on}")
+        
+    def switch_off(self, is_off):
+        print(f"The lamp is {is_off}")
 
-input_two = input('Enter the intercept of y-axis: ')
-y_intercept = int(input_two)
-
-input_three = input('Enter the value of x-coordinate: ')
-x_coordinate = int(input_three)
-
-y_coordinate = (line_slope * x_coordinate) + y_intercept
-
-print(f'The y-coordinate is: {y_coordinate} when the ' 
-      f'slope of the line is {line_slope}. '
-      f'Intercept of y-axis is {y_intercept} ' 
-      f'and X coordinate is: {x_coordinate}')
-
-'''
-The y-coordinate is: 23 when the slope of the line is 5. 
-Intercept of y-axis is 3 and X coordinate is: 4
-'''
+def main():
+      lamp = DesktopLamp()
+      lamp.switch_on("switched on")
+      # The lamp is switched on
+      lamp.switch_off("switched off")
+      # The lamp is switched off
+      
+if __name__ == '__main__': main()
