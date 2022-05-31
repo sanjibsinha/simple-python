e9df6d8ce17485714be5da881dbeab1b6a61bafd
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-31 17:26:04 +0530

    More NumPy

--- fifth.py
+++ fifth.py
@@ -1,14 +1,13 @@
-import pandas as panda
-import matplotlib.pyplot as plt
+import numpy as np
 
-data_frame_one = panda.DataFrame({'axis': ['X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y'], 
-                      'value': [2, 4, 4, 6, 6, 8, 8, 10]})
+list_of_numbers = [2, 4, 4, 4, 5, 5, 7, 9, 10]
 
-axis = data_frame_one['axis']
-
-value = data_frame_one['value']
-
-plt.plot(axis, value)
-plt.show()
-
-
+mean = np.mean(list_of_numbers)
+# The Mean is 5.555555555555555
+print(f'The Mean is {mean}')
+variance = np.var(list_of_numbers)
+print(f'The Variance is {variance}')
+# The Variance is 6.024691358024692
+standard_deviation = np.std(list_of_numbers)
+print(f'The Standard deviation is: {standard_deviation}') 
+# The Standard deviation is: 2.454524670486058
