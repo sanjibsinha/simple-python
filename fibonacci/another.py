62fc7b286ae91a140a9ce19149e295d4f4b316e4
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-09-08 16:40:41 +0530

    module intro name

--- fibonacci/another.py
+++ fibonacci/another.py
@@ -6,6 +6,8 @@ print(fib.return_Fibonacci_series(10).__doc__)
 fib.return_Fibonacci_series(100)
 print(fib.return_Fibonacci_series(100).__doc__)
 
+print(fib.__name__)
+
 '''
 0 1 1 2 3 5 8 
 [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -17,4 +19,6 @@ Built-in mutable sequence.
 
 If no argument is given, the constructor creates a new empty list.
 The argument must be an iterable if specified.
+
+fib
 '''
